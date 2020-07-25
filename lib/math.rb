2dad081d001bd6ab7ede41d1d25df2aa28089495
Mike Archer <mlarcher531@gmail.com>
2020-07-25 18:53:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -13,17 +13,32 @@ end
 subtraction(5,3)
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  quotient.to_i
 end
+
+division(6,2)
+
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum.to_i
 end
+
+multiplication(5,5)
 
 def modulo(num1, num2)
-
+  remainder = num1%num2
+  remainder.to_i
 end
+
+modulo(5,2)
+
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  root.to_i
 end
+
+square_root(9)
+
